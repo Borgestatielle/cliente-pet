9ffd70293c3f43035d4742c127a0b3826c6ab66e
Tatielle Borges <borgestatielle25@gmail.com>
2023-07-01 16:57:37 -0300

    implementa pet domain

--- src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
+++ src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
@@ -21,26 +21,26 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 public class Pet {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(columnDefinition = "uuid", name = "idPet", updatable = false, unique = true, nullable = false)
-	private UUID idCliente;
-	@NotBlank
-	private String nomePet;
-	@Enumerated(EnumType.STRING)
-	private Porte porte;
-	@Enumerated(EnumType.STRING)
-	private TipoPet tipo;
-	private String microchip;
-	private String raca;
-	@Enumerated(EnumType.STRING)
-	private SexoPet sexo;
-	private String pelagemCor;
-	@NotNull
-	private LocalDate dataNascimento;
-	private String rga;
-	private Integer peso;
-	
-	private LocalDateTime dataHoraDoCadastro;
-	private LocalDateTime dataHoraDaUltimaAlteracao;
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		@Column(columnDefinition = "uuid", name = "idPet", updatable = false, unique = true, nullable = false)
+		private UUID idPet;
+		@NotBlank
+		private String nomePet;
+		@Enumerated(EnumType.STRING)
+		private Porte porte;
+		@Enumerated(EnumType.STRING)
+		private TipoPet tipo;
+		private String microchip;
+		private String raca;
+		@Enumerated(EnumType.STRING)
+		private SexoPet sexo;
+		private String pelagemCor;
+		@NotNull
+		private LocalDate dataNascimento;
+		private String rga;
+		private Integer peso;
+		
+		private LocalDateTime dataHoraDoCadastro;
+		private LocalDateTime dataHoraDaUltimaAlteracao;
 }
--- src/main/java/br/com/petz/clientepet/pet/domain/SexoPet.java
+++ src/main/java/br/com/petz/clientepet/pet/domain/SexoPet.java
@@ -1,5 +1,5 @@
 package br.com.petz.clientepet.pet.domain;
 
 public enum SexoPet {
-FÊMEA, MACHO;
+FEMEA, MACHO;
 }
